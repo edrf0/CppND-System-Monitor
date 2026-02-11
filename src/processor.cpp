4a25055977939e241cc00f37b04d20dd3c6f5981
edrf0 <125705907+edrf0@users.noreply.github.com>
2026-02-11 00:09:49 -0400

    .

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,17 @@
 #include "processor.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+#include "linux_parser.h"
+
+float Processor::Utilization() {
+  long total_time = LinuxParser::Jiffies();
+  long idle_time = LinuxParser::IdleJiffies();
+
+  long delta_total = total_time - prev_total_time;
+  long delta_idle = idle_time - prev_idle_time;
+
+  // Updating for next call
+  prev_total_time = total_time;
+  prev_idle_time = idle_time;
+
+  return static_cast<float>(delta_total - delta_idle) / delta_total;
+}
