227ac424ddcba8b5cf8fe54f99c7c095a40adb00
edrf0 <125705907+edrf0@users.noreply.github.com>
2026-02-11 00:23:10 -0400

    .

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,16 @@
-#include <string>
-
 #include "format.h"
+
+#include <sstream>
+#include <string>
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  int hours = seconds / 3600;
+  int minutes = (seconds % 3600) / 60;
+  int _seconds = (seconds % 60);
+
+  std::ostringstream ostringstream;
+  ostringstream << hours << "h:" << minutes << "m:" << _seconds;
+  return ostringstream.str();
+}
